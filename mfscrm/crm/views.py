4f46665914fa48e62d8ed493d9d89f8040ec0080
sboddupalliuno <sboddupalli@unomaha.edu>
2020-03-13 11:58:43 -0500

    Initial Commit

---
+++ mfscrm/crm/views.py
@@ -0,0 +1,196 @@
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render
+from .models import *
+from .forms import *
+from django.shortcuts import render, get_object_or_404
+from django.shortcuts import redirect
+from django.db.models import Sum
+from django.urls import reverse_lazy
+from django.contrib.auth import views as auth_views, forms as auth_forms
+from django.conf import settings
+from django.http import HttpResponse
+from django.template.loader import render_to_string
+from django.template.loader import get_template
+from django.template import Context
+import pdfkit
+import os
+
+now = timezone.now()
+def home(request):
+   return render(request, 'crm/home.html',
+                 {'crm': home})
+
+@login_required
+def customer_list(request):
+    customer = Customer.objects.filter(created_date__lte=timezone.now())
+    return render(request, 'crm/customer_list.html',
+                  {'customers': customer})
+
+@login_required
+def customer_edit(request, pk):
+   customer = get_object_or_404(Customer, pk=pk)
+   if request.method == "POST":
+       # update
+       form = CustomerForm(request.POST, instance=customer)
+       if form.is_valid():
+           customer = form.save(commit=False)
+           customer.updated_date = timezone.now()
+           customer.save()
+           return redirect('crm:customer_list')
+   else:
+        # edit
+       form = CustomerForm(instance=customer)
+   return render(request, 'crm/customer_edit.html', {'form': form})
+
+@login_required
+def customer_delete(request, pk):
+   customer = get_object_or_404(Customer, pk=pk)
+   customer.delete()
+   return redirect('crm:customer_list')
+
+@login_required
+def service_list(request):
+   services = Service.objects.filter(created_date__lte=timezone.now())
+   return render(request, 'crm/service_list.html', {'services': services})
+
+@login_required
+def service_new(request):
+   if request.method == "POST":
+       form = ServiceForm(request.POST)
+       if form.is_valid():
+           service = form.save(commit=False)
+           service.created_date = timezone.now()
+           service.save()
+           return redirect('crm:service_list')
+   else:
+       form = ServiceForm()
+       # print("Else")
+   return render(request, 'crm/service_new.html', {'form': form})
+
+@login_required
+def service_edit(request, pk):
+   service = get_object_or_404(Service, pk=pk)
+   if request.method == "POST":
+       form = ServiceForm(request.POST, instance=service)
+       if form.is_valid():
+           service = form.save()
+           # service.customer = service.id
+           service.updated_date = timezone.now()
+           service.save()
+           return redirect('crm:service_list')
+   else:
+       # print("else")
+       form = ServiceForm(instance=service)
+   return render(request, 'crm/service_edit.html', {'form': form})
+
+@login_required
+def service_delete(request, pk):
+   service = get_object_or_404(Service, pk=pk)
+   service.delete()
+   return redirect('crm:service_list')
+
+@login_required
+def product_list(request):
+   products = Product.objects.filter(created_date__lte=timezone.now())
+   return render(request, 'crm/product_list.html', {'products': products})
+
+@login_required
+def product_new(request):
+   if request.method == "POST":
+       form = ProductForm(request.POST)
+       if form.is_valid():
+           product = form.save(commit=False)
+           product.created_date = timezone.now()
+           product.save()
+           return redirect('crm:product_list')
+   else:
+       form = ProductForm()
+       # print("Else")
+   return render(request, 'crm/product_new.html', {'form': form})
+
+@login_required
+def product_edit(request, pk):
+   product = get_object_or_404(Product, pk=pk)
+   if request.method == "POST":
+       form = ProductForm(request.POST, instance=product)
+       if form.is_valid():
+           product = form.save()
+           # service.customer = service.id
+           product.updated_date = timezone.now()
+           product.save()
+           return redirect('crm:product_list')
+   else:
+       # print("else")
+       form = ProductForm(instance=product)
+   return render(request, 'crm/product_edit.html', {'form': form})
+
+@login_required
+def product_delete(request, pk):
+   product = get_object_or_404(Product, pk=pk)
+   product.delete()
+   return redirect('crm:product_list')
+
+@login_required
+def summary(request, pk):
+    customer = get_object_or_404(Customer, pk=pk)
+    customers = Customer.objects.filter(created_date__lte=timezone.now())
+    services = Service.objects.filter(cust_name=pk)
+    products = Product.objects.filter(cust_name=pk)
+    sum_service_charge = Service.objects.filter(cust_name=pk).aggregate(Sum('service_charge'))
+    sum_product_charge = Product.objects.filter(cust_name=pk).aggregate(Sum('charge'))
+
+    return render(request, 'crm/summary.html', {'products': products,
+                                                'services': services,
+                                                'sum_service_charge': sum_service_charge,
+                                                'sum_product_charge': sum_product_charge,
+                                                'thecustomer':customer,
+                                                })
+
+@login_required
+def customer_pdf(request, pk):
+    customer = get_object_or_404(Customer, pk=pk)
+    customers = Customer.objects.filter(created_date__lte=timezone.now())
+    services = Service.objects.filter(cust_name=pk)
+    products = Product.objects.filter(cust_name=pk)
+    sum_service_charge = Service.objects.filter(cust_name=pk).aggregate(Sum('service_charge'))
+    sum_product_charge = Product.objects.filter(cust_name=pk).aggregate(Sum('charge'))
+    template = get_template('crm/summary.html')
+    html = template.render({'products': products,
+                                        'services': services,
+                                        'sum_service_charge': sum_service_charge,
+                                        'sum_product_charge': sum_product_charge,
+                                        'thecustomer':customer,
+                                        })
+    path_wkhtmltopdf = r'C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe'
+    config = pdfkit.configuration(wkhtmltopdf=path_wkhtmltopdf)
+    options = {
+        'page-size': 'Letter',
+        'encoding': "UTF-8",
+    }
+    pdf = pdfkit.from_string(html, False, options, configuration=config)
+    response = HttpResponse(pdf, content_type='application/pdf')
+    response['Content-Disposition'] = 'attachment; filename=customer' + str(pk) + '.pdf'
+    return response
+
+
+
+class PasswordResetView(auth_views.PasswordResetView):
+    form_class = auth_forms.PasswordResetForm
+    template_name = 'crm/reset_password.html'
+    email_template_name = 'crm/reset_password_email.html'
+    success_url = reverse_lazy('crm:reset_password_done')
+
+class PasswordResetDoneView(auth_views.PasswordResetDoneView):
+    form_class = auth_forms.PasswordResetForm
+    template_name = 'crm/reset_password_done.html'
+    #success_url = reverse_lazy('reset_password_done')
+
+class PasswordResetConfirmView(auth_views.PasswordResetConfirmView):
+    form_class = auth_forms.SetPasswordForm
+    template_name = 'crm/reset_password_confirm.html'
+    success_url = reverse_lazy('crm:reset_password_complete')
+
+class PasswordResetCompleteView(auth_views.PasswordResetCompleteView):
+    form_class = auth_forms.PasswordResetForm
+    template_name = 'crm/reset_password_complete.html'
+    #success_url = reverse_lazy('login.html')
